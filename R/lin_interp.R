e2551baa19fc2f444cd3d3f5f882eb310af0d030
Chi-Kuang <chi-kuang.yeh@uwaterloo.ca>
2020-08-05 14:17:03 -0400

    add - function: lin_interp to do the linear interpolation for a probabilistic forecast

---
+++ R/lin_interp.R
@@ -0,0 +1,21 @@
+#' Linear Interpolation
+#'
+#' lin_interp() returns the set of input probabilistic forecasts evaluated on the given grid
+#'
+#' @param prob TBA
+#' @param grid TBA
+#' @param outcome TBA
+#'
+#' @return A tibble contains a set of interpolated probabilistic forecasts, outcome variable and grid.
+#'
+#' @export
+
+
+lin_interp <- function(prob, grid, outcome){
+  ngrid  <- length(grid)
+  my_grid <- seq(0, 1, 1 / ngrid)
+  df <- tibble(prob = prob, grid = grid)
+
+  df %>% summarise(phat_approx = list(approx(grid, prob, my_grid, method = "linear")$y),
+                   grid = list(my_grid), Y = outcome) %>% unnest_legacy()
+}
